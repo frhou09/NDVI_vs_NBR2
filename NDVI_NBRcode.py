e68f9b449f4007fb183ba4aece816f19eccbc5c5
frhou09 <francinehou6@gmail.com>
2026-07-16 11:34:37 -0400

    calculation

---
+++ NDVI_NBRcode.py
@@ -0,0 +1,53 @@
+import os
+import glob
+import rasterio
+import numpy as np
+
+#Using glob to find bands
+scene = "/home/francine/hermits_peak_data/raw_scenes/LC80320352025247LGN00"
+
+b4_path = glob.glob(f"{scene}/*_02_T1_SR_B4.TIF")
+b5_path = glob.glob(f"{scene}/*_02_T1_SR_B5.TIF")
+b7_path = glob.glob(f"{scene}/*_02_T1_SR_B7.TIF")
+
+
+#opening the bands
+with rasterio.open(b4_path [0])as b4_src, \
+    rasterio.open(b5_path [0])as b5_src, \
+    rasterio.open(b7_path [0])as b7_src:  
+
+#Copy b5 map coordinates into meta clone
+    meta=b5_src.meta.copy()
+
+    b4 = b4_src.read(1).astype('float32')
+    b5 = b5_src.read(1).astype('float32')
+    b7 = b7_src.read(1).astype('float32')
+
+#NDVI calculation
+    NDVI = (b5 - b4)/(b5 + b4)
+
+#NBR calculation
+    NBR = (b5 - b7)/(b5 + b7)
+
+print(np.nanmin(NDVI), np.nanmax(NDVI), np.nanmean(NDVI))
+
+#Save as single layer map
+meta.update(dtype=rasterio.float32, count=1)
+
+
+#Save as TIF
+with rasterio.open(f"{scene}/output_NDVI.tif", 'w', **meta) as dst_NDVI:
+        dst_NDVI.write(NDVI, 1)
+
+with rasterio.open(f"{scene}/output_NBR.tif", 'w', **meta) as dst_NBR:
+        dst_NBR.write(NBR, 1)
+
+
+
+
+
+
+
+
+
+
